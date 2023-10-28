ea8eaf27d988fa462f8cf082a4c8997a9c51b041
Shivaraj <117928514+shivarajkulal@users.noreply.github.com>
2023-10-28 09:51:30 +0530

    Add files via upload

---
+++ WordCount.java
@@ -0,0 +1,14 @@
+/*
+word count in string
+*/
+import java.util.Scanner;
+public class CountTheWords {
+public static void main(String[] args)
+{
+System.out.println("Enter the string");
+Scanner sc = new Scanner(System.in);
+String s=sc.nextLine();
+String[] words = s.trim().split(" ");
+System.out.println("Number of words in the string ="+words.length);
+}
+}
